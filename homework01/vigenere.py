41e059220c3a8efeeafd4d83288b412e602b977f
Ksenia <ksu.ksu-2@mail.ru>
2019-10-12 16:31:14 +0300

    Реализована функция decrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -20,11 +20,11 @@ def encrypt_vigenere(plaintext, keyword):
         if ('A' <= letter <= 'Z') or ('a' <= letter <= 'z'):
             a = (ord(letter) % 32) - 1
             a += (ord(keyword[number]) % 32) - 1
-            a = a % 26 + 1
-            if letter <= 'Z':
-                newletter = chr(a + 64)
+            a = a % 26
+            if 'A' <= letter <= 'Z':
+                newletter = chr(a + ord('A'))
             else:
-                newletter = chr(a + 96)  
+                newletter = chr(a + ord('a'))  
         else:
             newletter = letter
         ciphertext += newletter
@@ -43,4 +43,25 @@ def decrypt_vigenere(ciphertext, keyword):
     'ATTACKATDAWN'
     """
     # PUT YOUR CODE HERE
+
+    plaintext = ""
+    temp = keyword
+    while len(keyword) < len(ciphertext):
+        keyword += temp
+    number = -1
+    for letter in ciphertext:
+        number += 1
+        if ('A' <= letter <= 'Z') or ('a' <= letter <= 'z'):
+            a = (ord(letter) % 32) - 1
+            a -= (ord(keyword[number]) % 32) - 1
+            a = a % 26
+            if 'A' <= letter <= 'Z':
+                newletter = chr(a + ord('A'))
+            else:
+                newletter = chr(a + ord('a'))  
+        else:
+            newletter = letter
+        plaintext += newletter
     return plaintext
+
+
