1cb9f374ad2b19d7208530c82df3c50e8f95070c
Ksenia <ksu.ksu-2@mail.ru>
2019-12-30 18:51:15 +0300

    добавлена функция поиска медианы

--- homework04/age.py
+++ homework04/age.py
@@ -1,10 +1,18 @@
 import datetime as dt
-from statistics import median
+#from statistics import median
 from typing import Optional
 
 from api import get_friends
 #from api_models import User
 
+def median(arr: list):
+    arr.sort()
+    if len(arr) % 2:
+        # для нечетного кол-ва
+        return float(arr[len(arr) // 2])
+    else:
+        # для четного кол-ва
+        return (float(arr[len(arr) // 2 - 1]) + float(arr[len(arr) // 2])) / 2
 
 def age_predict(user_id: int) -> Optional[float]:
     """ Наивный прогноз возраста по возрасту друзей
